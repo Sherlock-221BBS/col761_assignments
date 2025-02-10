af6e28978fdcb167a8c94df942306dfeb23a1365
utkarsh <56836753+utkarsh299@users.noreply.github.com>
2025-02-10 23:34:20 +0530

    updated env for creating conda env

--- hw1/q3/env.sh
+++ hw1/q3/env.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+eval "$(conda shell.bash hook)"
+
+if ! conda env list | grep -q "whocares"; then
+    echo "Creating Conda environment 'whocares'..."
+    conda create -n whocares python=3.10 -y
+else
+    echo "Conda environment 'whocares' already exists. Skipping creation."
+fi
+
+conda activate whocares
+
 pip install numpy scikit-learn networkx==2.8.8
+
 chmod +x gaston
 chmod +x convert.sh
 chmod +x identify.sh
